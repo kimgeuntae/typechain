d0b0bf7c730d84c3b18c9c69b4dcdf1bb85fff30
kimgeuntae <kok617@naver.com>
2019-11-18 19:29:54 +0900

    Add [Interface , Class]

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+/*
+interface Human {
+    name: string,
+    age: number,
+    gender: string
+}   // Interface is not include in compiled JS code.
+
 const person = {
     name: "kimgeuntae",
     age: 22,
     gender: "male"
-};
+}
+*/
+const kim = new Human("Tay", 21, "male");
 const sayHi = (person) => {
     return `hello ${person.name}, your are ${person.age}, you are a ${person.gender}!`;
 };
-console.log(sayHi(person));
+console.log(sayHi(kim));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+/*
 interface Human {
     name: string,
     age: number,
     gender: string
-}
+}   // Interface is not include in compiled JS code.
 
 const person = {
     name: "kimgeuntae",
     age: 22,
     gender: "male"
 }
+*/
 
-const sayHi = (person: Human): string=> {
+const kim = new Human("Tay", 21, "male");
+
+const sayHi = (person: Human): string => {
     return `hello ${person.name}, your are ${person.age}, you are a ${person.gender}!`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(kim));
 
-export {};
+export { };
